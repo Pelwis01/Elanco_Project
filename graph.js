2c4a6b45e65ece9d9f4e527e520209fea5812d3f
Mark Staniforth <staniforthmark4@gmail.com>
2026-03-25 13:49:14 +0000

    added the graph to the page

--- graph.js
+++ graph.js
@@ -1,5 +1,7 @@
-const xArray = [50,60,70,80,90,100,110,120,130,140,150];
-const yArray = [7,8,8,9,9,9,10,11,14,14,15];
+const plotbox = document.getElementById("myPlot");
+
+const xArray = [50,60,70,80,90,100,110,120,130,140,1500];
+const yArray = [7,8,8,9,9,9,10,11,14,14,150];
 
 // Define Data
 const data = [{
@@ -10,10 +12,10 @@ const data = [{
 
 // Define Layout
 const layout = {
-  xaxis: {range: [40, 160], title: "Square Meters"},
-  yaxis: {range: [5, 16], title: "Price in Millions"},  
-  title: "House Prices vs. Size"
+  xaxis: {range: [1, 72], title: "Hour"},
+  yaxis: {range: [0, 100], title: "Percentage Chance of parasite infection"},  
+  title: "Chance of parasite infection over time"
 };
 
 // Display using Plotly
-Plotly.newPlot("myPlot", data, layout);
+Plotly.newPlot(plotbox, data, layout);
